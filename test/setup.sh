5503f0adecbe035c250858129e71c9eb0a8b91a1
Nathan Friedly <nathan@nfriedly.com>
2017-02-21 20:31:59 -0500

    esling ignore docpad

--- test/setup.sh
+++ test/setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# when testing plugins, docpad must be installed from source and linked to - the npm version is missing things
+# per instructions at https://github.com/docpad/documentation/blob/master/extend/plugin-write.html.md#running-the-tests
+
 git clone https://github.com/bevry/docpad.git
 cd docpad
 npm install
