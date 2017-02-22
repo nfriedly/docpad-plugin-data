c6588d16e40c3d9b74b9ef51fa71b40a48ccc9a9
Nathan Friedly <nathan@nfriedly.com>
2017-02-21 23:54:19 -0500

    smart reloading and variable names. Fixes #2

--- README.md
+++ README.md
@@ -8,21 +8,30 @@ DocPad plugin for simple data files. Loads and parses JSON, YAML, JavaScript, an
 stores the contents on `config.templateData`, which makes the data available at `@filename` when rendering documents.
 
 Forked from [docpad-plugin-data](https://github.com/SE7ENSKY/docpad-plugin-data) with several improvements:
-* Better handling of filenames - now allows for dashes and other characters (use `@['file-name']` to reference the data then.)
+* Support for files with spaces, hyphens, etc. in the name (e.g. `file-name.json becomes `@fileName`.)
 * Support for absolute folder paths
+* Data is cached and only updated when the source file changes
 * No annoying error about uncompiled CoffeeScript
 * Documentation
 * Tests
 
 ## Installation
 
-    docpad install datafiles
+```sh
+docpad install datafiles
+```
 
 ## Configuration
 
-**dataPaths**: An array of folders to search for data files in. Relative `dataPaths` are resolved from `docpadConfig.srcPath`, which defaults to `./src/`.
+**dataPaths**: An array of folders to search for data files in. 
+Relative `dataPaths` are resolved from `docpadConfig.srcPath`, which defaults to `./src/`.
 
-**alwaysReload**: When false, data files will only be loaded once at DocPad start. When true, all files will be reloaded before each regeneration.
+**camelCase**: converts file names with spaces, hyphens, etc. to camelCase to enable easier usage in templates.
+For example `foo-bar.json` becomes `@fooBar` when `true` vs `@['foo-bar']` when false.
+
+**alwaysReload**: Default behavior is to check the `mtime` and only reload modified files during regeneration. 
+When true, this check will be skipped and all files will always be reloaded. 
+Note that DocPad only watches for changes in folders under `docpadConfig.srcPath`.
 
 
 Config with default options in CoffeeScript:
@@ -32,6 +41,7 @@ docpadConfig = {
   plugins: {
     datafiles: 
       dataPaths: [ 'data' ],
+      camelCase: true,
       alwaysReload: false
   }    
   # ...
@@ -45,6 +55,7 @@ const docpadConfig = {
   plugins: {
     datafiles: {
       dataPaths: ['data'],
+      camelCase: true
       alwaysReload: false
     }
   }
--- src/datafiles.plugin.js
+++ src/datafiles.plugin.js
@@ -3,8 +3,9 @@
 // based on https://github.com/SE7ENSKY/docpad-plugin-data
 
 const fs = require('fs');
-const YAML = require('yamljs');
 const path = require('path');
+const YAML = require('yamljs');
+const camelCase = require('camelcase');
 
 module.exports = function(BasePlugin) {
   /**
@@ -16,8 +17,11 @@ module.exports = function(BasePlugin) {
 
     config: {
       dataPaths: ['data'],
-      alwaysReload: true
+      camelCase: true,
+      alwaysReload: false
     },
+
+    mtimeCache: {},
 
     /**
      * extendTemplateData event hook
@@ -44,16 +48,20 @@ module.exports = function(BasePlugin) {
           const dataFiles = fs.readdirSync(dataPath);
           dataFiles.forEach(dataFile => {
             const fullFilePath = path.join(dataPath, dataFile);
+            if (!config.alwaysReload) {
+              const mtime = fs.statSync(fullFilePath).mtime.getTime();
+              if (this.mtimeCache[fullFilePath] === mtime) {
+                // this file is unmodified since the last load, skip it
+                return;
+              }
+              this.mtimeCache[fullFilePath] = mtime;
+            }
             const ext = path.extname(dataFile);
-            const name = path.basename(dataFile, ext);
+            let name = path.basename(dataFile, ext);
+            if (config.camelCase) {
+              name = camelCase(name);
+            }
             switch (ext) {
-              case '.json':
-                templateData[name] = JSON.parse(
-                  fs.readFileSync(fullFilePath, {
-                    encoding: 'utf8'
-                  })
-                );
-                break;
               case '.yaml':
               case '.yml':
                 templateData[name] = YAML.parse(
@@ -63,6 +71,7 @@ module.exports = function(BasePlugin) {
                 );
                 break;
               case '.coffee':
+              case '.json':
               case '.js':
                 delete require.cache[require.resolve(fullFilePath)];
                 templateData[name] = require(fullFilePath);
@@ -87,13 +96,7 @@ module.exports = function(BasePlugin) {
      * @return {DataFilesPlugin}
      */
     renderBefore: function(opts) {
-      const docpadConfig = this.docpad.getConfig();
-      const config = docpadConfig.plugins.datafiles || {};
-      if (config.alwaysReload) {
-        return this.extendTemplateData(opts);
-      } else {
-        return this;
-      }
+      return this.extendTemplateData(opts);
     }
   });
 
